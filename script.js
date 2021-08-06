0129df2cbdd5f0b3725804a302266e941539db93
Sergey <sergeyxyzz@yandex.ru>
2021-08-06 11:45:24 +0300

    add cycle&condition

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
+const numberOfFilms = prompt('Сколько фильмов Вы посмотрели?', '')
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +10,26 @@ const personalMovieDB = {
     privat: false
 }
 
-const a = prompt('один из просмотренных фильмов?', ''),
-      b = +prompt('на сколько оцените его?', ''),
-      c = prompt('один из просмотренных фильмов?', ''),
-      d = +prompt('на сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из просмотренных фильмов?', ''),
+          b = +prompt('на сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+if (personalMovieDB.count <= 10) {
+    console.log('просмотрено мало фильмов')
+} else if (personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
 
 
 console.log(personalMovieDB)
