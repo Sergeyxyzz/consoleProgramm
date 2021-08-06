bfb2e5bada426cfb604d9e4e7ea92964325cbda3
Sergey <sergeyxyzz@yandex.ru>
2021-08-06 06:42:58 +0300

    first task, add prompt in obj

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?')
+
+let a = prompt('один из просмотренных фильмов?', ''),
+    b = +prompt('на сколько оцените его?', ''),
+    c = prompt('один из просмотренных фильмов?', ''),
+    d = +prompt('на сколько оцените его?', '');
+
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {
+        [a]: b,
+        [c]: d
+    },
     actors: {},
     genres: [],
     privat: false
 }
 
+console.log(personalMovieDB)
