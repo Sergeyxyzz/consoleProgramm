3e3ef7fca5ac208ddd702e00c825c41d11f0251a
Sergey <sergeyxyzz@yandex.ru>
2021-08-06 05:27:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
